f5926fddb54837538cfb2600f40d901b849c060f
machikawa <a1938hm@aiit.ac.jp>
2024-10-22 19:16:43 +0900

    Initial Commit

---
+++ MachiLearningApp/MachiLearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MachiLearningAppApp.swift
+//  MachiLearningApp
+//
+//  Created by たうんりばーVR on 2024/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MachiLearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MachiLearningAppTests/MachiLearningAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MachiLearningAppTests.swift
+//  MachiLearningAppTests
+//
+//  Created by たうんりばーVR on 2024/10/22.
+//
+
+import Testing
+@testable import MachiLearningApp
+
+struct MachiLearningAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
